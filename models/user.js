1f1524180a4181d1f5586c8914c022b1cfe89a45
Steve Dongich <stevowny@gmail.com>
2024-07-29 23:32:27 +0300

    creating mongodb and schema

---
+++ models/user.js
@@ -0,0 +1,17 @@
+import mongoose, { models, Schema } from "mongoose";
+
+export const userSchema = new Schema({
+    email:{
+        type: String,
+        required: true
+    },
+    name: {
+        type:String,
+        required:true
+    }
+},
+{timestamps:true}
+
+)
+const User = models.User || mongoose.model("User", userSchema)
+export default User;
